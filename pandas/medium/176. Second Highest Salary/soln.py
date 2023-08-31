72b3ad8e5db7da1fa14585aca1aa258a5c7c9957
respice-finem <twyb98@gmail.com>
2023-08-31 23:54:41 +0800

    176. Second Highest Salary

---
+++ pandas/medium/176. Second Highest Salary/soln.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+
+    """
+    Logic works the same
+
+    1. We can drop duplicates first
+    2. Sort or find out the nlargest values
+    3. Identify the second largest or else None
+    4. Create new Dataframe for it
+    """
+
+    second_highest = None
+    unique = employee['salary'].drop_duplicates()
+    if len(unique) > 1:
+        second_highest = unique.nlargest(2).iloc[1]
+
+    return pd.DataFrame({"SecondHighestSalary": [second_highest]})
