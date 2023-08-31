bafc38921c905ece144d8a8a66b3b9051d42078c
respice-finem <twyb98@gmail.com>
2023-08-31 23:58:28 +0800

    Changes to 176

--- pandas/medium/176. Second Highest Salary/soln.py
+++ pandas/medium/176. Second Highest Salary/soln.py
@@ -11,9 +11,19 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     4. Create new Dataframe for it
     """
 
+    """Solution 1"""
     second_highest = None
     unique = employee['salary'].drop_duplicates()
     if len(unique) > 1:
         second_highest = unique.nlargest(2).iloc[1]
+
+    return pd.DataFrame({"SecondHighestSalary": [second_highest]})
+
+    """Solution 2"""
+    second_highest = None
+    unique = employee['salary'].drop_duplicates().sort_values(ascending=False)
+
+    if len(unique) > 1:
+        second_highest = unique.iloc[1]
 
     return pd.DataFrame({"SecondHighestSalary": [second_highest]})
