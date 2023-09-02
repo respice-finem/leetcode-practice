fdff6b416152a2f9f7514a4b4cf516b9a7e7e7ae
respice-finem <twyb98@gmail.com>
2023-09-03 00:34:26 +0800

    226. Invert Binary Tree

---
+++ algo/easy/226. Invert Binary Tree/soln.py
@@ -0,0 +1,34 @@
+def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    """
+    Time Complexity: O(n) --> Go through every single node
+    Space Complexity: O(1) --> Reusing same tree
+
+    TODO:
+    1. Start from root
+    2. Recursively go down and switch the nodes
+    3. Until none, stop operation
+    """
+    if root:
+        root.left, root.right = root.right, root.left # Make use of Python swap syntax
+        invertTree(root.left)
+        invertTree(root.right)
+    return root
+
+    """
+    Cleaner Implementation
+
+    Time Complexity: O(n) --> Go through every single node
+    Space Complexity: O(1) --> Reusing same tree
+
+    TODO:
+    1. Start from root
+    2. Recursively go down and switch the nodes
+    3. Until none, stop operation
+    """
+    if not root:
+        return root
+    root.left, root.right = root.right, root.left
+    for node in [root.left, root.right]:
+        if node: # Remove unnecessary call stacks (Slight improvement)
+            invertTree(node)
+    return root
